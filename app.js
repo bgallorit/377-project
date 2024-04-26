aa1ed3045827069e572ee0a2fe143b81c1c81120
bgallorit <bpg3891@rit.edu>
2024-04-26 11:13:19 -0400

    added special conditions for snooze

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let data = {
-    'init': ['You wake up to your alarm blaring. It is 8:30. You have to be in class in an hour.', 'Get up', 'Snooze'],
-    'Get up': ['You lift yourself out of bed. You should probably take a shower. ', 'Take shower', 'Skip the shower'],
-    'Take shower': ['After taking a shower, you make your way to class.'],
-    'Skip the shower': ["You decide it's probably fine to skip the shower... hopefully..."],
-    'Snooze': ['You decide to give yourself just five more minutes of sleep. Those five minutes go by seeminly instantly, and your alarm goes off again.', 'Get up', 'Snooze again'],
-    'Snooze again': ["Another five minutes couldn't hurt. Just as you close your eyes, you hear your alarm again.", 'Get up', 'Snooze once again'],
-    'Snooze once again': ["You feel very tired... five more minutes... the alarm beeps again...", 'Get up', 'Snooze...'],
-    'Snooze...': ["You don't even remember if you closed your eyes before you hear your alarm for the fourth time.", 'Get up', 'More sleep...'],
-    'More sleep...': ["You're not even sure if you can muster up the energy to physically get up now.", 'Get up', '...'],
-    '...': ['Instead of hitting the snooze button, you accidentally turn your alarm off. You fall into a deep sleep.']
+    'init': ['initid', 'You wake up to your alarm blaring. It is 8:30. You have to be in class in an hour.', 'Get up', 'Snooze'],
+    'Get up': ['wakeup', 'You lift yourself out of bed. You should probably take a shower. ', 'Take shower', 'Skip the shower'],
+    'Take shower': ['shower', 'After taking a shower, you make your way to class.'],
+    'Skip the shower': ['noshower', "You decide it's probably fine to skip the shower... hopefully..."],
+    'Snooze1': ['snooze1', 'You decide to give yourself just five more minutes of sleep. Those five minutes go by seeminly instantly, and your alarm goes off again.', 'Get up', 'Snooze'],
+    'Snooze2': ['snooze2', "Another five minutes couldn't hurt. Just as you close your eyes, you hear your alarm again.", 'Get up', 'Snooze'],
+    'Snooze3': ['snooze3', "You feel very tired... five more minutes... the alarm beeps again...", 'Get up', 'Snooze'],
+    'Snooze4': ['snooze4', "You don't even remember if you closed your eyes before you hear your alarm for the fourth time.", 'Get up', 'Snooze'],
+    'Snooze5': ['snooze5', "You're not even sure if you can muster up the energy to physically get up now.", 'Get up', 'Snooze'],
+    'Snooze6': ['snooze6', 'Instead of hitting the snooze button, you accidentally turn your alarm off. You fall into a deep sleep.']
 
     // 'Choice 1': ['You selected choice 1! What now?', 'Choice 1A', 'Choice 1B'],
     // 'Choice 2': ['You selected choice 2! What now?', 'Choice 2A', 'Choice 2B'],
@@ -19,9 +19,10 @@ let data = {
 }
 
 let currentChoice;
+let snoozeCount = 0;
 
 function init() {       //initial text and button creation, doesn't need to be dynamic
-    document.getElementById('textbox').innerText = data.init[0];
+    document.getElementById('textbox').innerText = data.init[1];
     let initButton1 = document.createElement('button');
     initButton1.innerText = "Get up";
     initButton1.onclick = function(){
@@ -39,13 +40,19 @@ function init() {       //initial text and button creation, doesn't need to be d
 function changeText(choice){
     console.log(choice);
     document.getElementById('choices').innerHTML = '';
-    document.getElementById('textbox').innerHTML = data[choice][0];
-    for(let i=1; i<data[choice].length; i++) {
+    if(choice == 'Snooze'){
+        snoozeCount++;
+        choice = choice + snoozeCount.toString();
+        console.log(choice);
+    }
+    document.getElementById('textbox').innerHTML = data[choice][1];
+    for(let i=2; i<data[choice].length; i++) {
         let button = document.createElement('button');
         button.innerText = data[choice][i];
         document.getElementById('choices').append(button);
         button.onclick = function(){
             changeText(button.innerText);
+            
         }
     }
 
